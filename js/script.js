39f066c46c0a55790e301206fc866f7d0e4539b6
Rico Guinanao <83864808+coricss@users.noreply.github.com>
2022-06-19 18:09:13 +0800

    Commented timer function

--- js/script.js
+++ js/script.js
@@ -23,60 +23,33 @@ const login_auth = (e) => {
     }
 }
 
-// var upgradeTime = 10800;
-// var seconds = upgradeTime;
 
-// function timer() {
-//   var days        = Math.floor(seconds/24/60/60);
-//   var hoursLeft   = Math.floor((seconds) - (days*86400));
-//   var hours       = Math.floor(hoursLeft/3600);
-//   var minutesLeft = Math.floor((hoursLeft) - (hours*3600));
-//   var minutes     = Math.floor(minutesLeft/60);
-//   var remainingSeconds = seconds % 60;
-//   function pad(n) {
-//     return (n < 10 ? "0" + n : n);
-//   }
-//   document.getElementById('countdown').innerHTML = pad(hours) + " : " + pad(minutes) + " : " + pad(remainingSeconds);
-//   if (seconds == 0) {
-//     clearInterval(countdownTimer);
-//     // $('.countdown').html("Time's Up");
-//     // $('#autoSubmit').val('submit');
-//     // $('#testForm').submit();
-//   } else {
-//     seconds--;
-//   }
-// }
-// var countdownTimer = setInterval(timer(), 1000);
+var seconds = 259200; //days to seconds
 
-var setTime = 259200;
-var seconds = setTime;
- var startTimer = () => {
-  var days        = Math.floor(seconds/24/60/60);
-  var hoursLeft   = Math.floor((seconds) - (days*86400));
-  var hours       = Math.floor(hoursLeft/3600);
-  var minutesLeft = Math.floor((hoursLeft) - (hours*3600));
-  var minutes     = Math.floor(minutesLeft/60);
-  var remainingSeconds = seconds % 60;
+var startTimer = () => {
+
+  var days        = Math.floor(seconds/24/60/60); //calculate days based in seconds
+  var hoursLeft   = Math.floor((seconds) - (days*86400)); //calculate hours left by multiplying days to 86400sec/1day and subtract to seconds
+  var hours       = Math.floor(hoursLeft/3600); //calculate hours by dividing hours left to 3600sec/1hr
+  var minutesLeft = Math.floor((hoursLeft) - (hours*3600)); //calculate minutes left by multiplying hours and 3600sec/1hr and subtract to hours left
+  var minutes     = Math.floor(minutesLeft/60); //calculate minutes by dividing minutes left to 60sec/1min
+  var remainingSeconds = seconds % 60; //calculate remaining seconds by getting the remainder of seconds in 1minute.
+  
   function pad(x) {
-    return (x < 10 ? "0" + x : x);
+    return (x < 10 ? "0" + x : x); //returning time number to 00 format, it changes when time number hits 10
   }
-  document.getElementById('countdown').innerHTML = pad(days) + " Days : " + pad(hours) + " Hrs : " + pad(minutes) + " Mins : " + pad(remainingSeconds) + " Sec";
+
+  document.getElementById('countdown').innerHTML = pad(days) + " Days : " + pad(hours) + " Hrs : " + pad(minutes) + " Mins : " + pad(remainingSeconds) + " Sec"; //setting countdown to span element
+
   if (seconds == 0) {
+     //if seconds equal to 0 timer will stop and change the countdown to battle pass ended
     clearInterval(countdownTimer);
     document.getElementById('countdown').innerHTML = "Battle Pass Ended";
   } else {
+     //seconds will continue to decrease
     seconds--;
   }
+
+
 }
-var countdownTimer = setInterval('startTimer()', 1000);
-
-// const myInterval = setInterval(myTimer, 1000);
-
-// function myTimer() {
-//   const date = new Date();
-//   document.getElementById("countdown").innerHTML = date.toLocaleTimeString();
-// }
-
-// function myStopFunction() {
-//   clearInterval(myInterval);
-// }
+var countdownTimer = setInterval('startTimer()', 1000); //setting the interval in 1 sec
