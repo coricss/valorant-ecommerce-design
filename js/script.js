346aa7b1be87a35c65a5b6a473494a17c2fc153a
Rico Guinanao <83864808+coricss@users.noreply.github.com>
2022-06-19 15:54:58 +0800

    Added countdown

--- js/script.js
+++ js/script.js
@@ -21,6 +21,62 @@ const login_auth = (e) => {
         username.classList.add('border-danger');
         password.classList.add('border-danger');
     }
-    
-
 }
+
+// var upgradeTime = 10800;
+// var seconds = upgradeTime;
+
+// function timer() {
+//   var days        = Math.floor(seconds/24/60/60);
+//   var hoursLeft   = Math.floor((seconds) - (days*86400));
+//   var hours       = Math.floor(hoursLeft/3600);
+//   var minutesLeft = Math.floor((hoursLeft) - (hours*3600));
+//   var minutes     = Math.floor(minutesLeft/60);
+//   var remainingSeconds = seconds % 60;
+//   function pad(n) {
+//     return (n < 10 ? "0" + n : n);
+//   }
+//   document.getElementById('countdown').innerHTML = pad(hours) + " : " + pad(minutes) + " : " + pad(remainingSeconds);
+//   if (seconds == 0) {
+//     clearInterval(countdownTimer);
+//     // $('.countdown').html("Time's Up");
+//     // $('#autoSubmit').val('submit');
+//     // $('#testForm').submit();
+//   } else {
+//     seconds--;
+//   }
+// }
+// var countdownTimer = setInterval(timer(), 1000);
+
+var setTime = 259200;
+var seconds = setTime;
+ var startTimer = () => {
+  var days        = Math.floor(seconds/24/60/60);
+  var hoursLeft   = Math.floor((seconds) - (days*86400));
+  var hours       = Math.floor(hoursLeft/3600);
+  var minutesLeft = Math.floor((hoursLeft) - (hours*3600));
+  var minutes     = Math.floor(minutesLeft/60);
+  var remainingSeconds = seconds % 60;
+  function pad(x) {
+    return (x < 10 ? "0" + x : x);
+  }
+  document.getElementById('countdown').innerHTML = pad(days) + " Days : " + pad(hours) + " Hrs : " + pad(minutes) + " Mins : " + pad(remainingSeconds) + " Sec";
+  if (seconds == 0) {
+    clearInterval(countdownTimer);
+    document.getElementById('countdown').innerHTML = "Battle Pass Ended";
+  } else {
+    seconds--;
+  }
+}
+var countdownTimer = setInterval('startTimer()', 1000);
+
+// const myInterval = setInterval(myTimer, 1000);
+
+// function myTimer() {
+//   const date = new Date();
+//   document.getElementById("countdown").innerHTML = date.toLocaleTimeString();
+// }
+
+// function myStopFunction() {
+//   clearInterval(myInterval);
+// }
